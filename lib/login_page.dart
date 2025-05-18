d43c30db761948ba64268e7b5f2d52860b59c606
sabel2-0 <markivan.note@gmail.com>
2025-05-18 13:05:35 +0800

    Navigating via named routes

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,11 +13,8 @@ class LoginPage extends StatelessWidget {
       print(userNameController.text);
       print(passwordController.text);
 
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => ChatPage(
-                username: userNameController.text,)));
+      Navigator.pushNamed(context,'/chat', 
+      arguments: '${userNameController.text}');
       print('login! successful!');
     } else {
       print('not successful');
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_app/counter_stateful_demo.dart';
 
 void main() {
   runApp(ChatApp());
@@ -11,13 +10,13 @@ void main() {
 class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       title: "Flutter Chat App!!!",
       // theme: ThemeData(primarySwatch: Colors.yellow),
       // home: CounterStateful(buttonColor:Colors.blue),
       theme: ThemeData(primarySwatch: Colors.deepPurple, appBarTheme: AppBarTheme(backgroundColor: Colors.blue, foregroundColor: Colors.black)),
       home: LoginPage(),
+      routes: {'/chat': (context) => ChatPage()},
     );
   }
 }
