2a8d03949a3dbfdad063bb3f7e87bba383b86181
sabel2-0 <markivan.note@gmail.com>
2025-05-18 15:42:17 +0800

    Adding packages from external sources

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,7 @@ import 'package:chat_app/utils/spaces.dart';
 import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
@@ -111,7 +112,21 @@ class LoginPage extends StatelessWidget {
                     Text(_mainUrl),
                   ],
                 ),
-              )
+              ),
+               Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialMediaButton.facebook(
+                      size: 20,
+                      color: Colors.blue,
+                      url: "https://www.facebook.com/share/1BPmT72qMA/",
+                    ),
+                    SocialMediaButton.linkedin(
+                      url:
+                          "https://www.linkedin.com/in/alvic-caranzo-b724a4170/",
+                    )
+                  ],
+                )
             ],
           ),
         ),
