bec59f7fe94488e4417866b84def54d9b2e8b612
sabel2-0 <markivan.note@gmail.com>
2025-05-20 09:31:38 +0800

    Change notifiers in Flutter

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -50,7 +50,8 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    final username = ModalRoute.of(context)!.settings.arguments as String;
+  final username = context.watch<AuthService>().getUserName();
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -60,6 +61,11 @@ class _ChatPageState extends State<ChatPage> {
           child: Text('Hi $username!'),
         ),
         actions: [
+          IconButton(
+              onPressed: () {
+                context.read<AuthService>().updateUserName("New Name!");
+              },
+              icon: Icon(Icons.logout)),
           IconButton(
               onPressed: () {
                 context.read<AuthService>().logoutUser();
